10cbcdf9534ed6ead27b0481d1014ec45f9ef861
Selim Yaman <mselimyaman@gmail.com>
2023-03-08 18:54:38 +0300

    arayüz oluşturuldu. search eklendi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MainContext from "./MainContext";
+import { useEffect, useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
+import BrandsData from "./brands.json";
+import Copied from "./components/Copied";
 
 function App() {
+  const brandsArray = [];
+  Object.keys(BrandsData).map((key) => {
+    brandsArray.push(BrandsData[key]);
+  });
+
+  const [brands, setBrands] = useState(brandsArray);
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [copied, setCopied] = useState(false);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCopied(false);
+    }, 1000)
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, [copied]);
+
+  useEffect(() => {
+    setBrands(brandsArray.filter(brand => brand.title.toLowerCase().includes(search.toLowerCase())))
+  }, [search])
+
+  const data = {
+    brands,
+    selectedBrands,
+    setSelectedBrands,
+    setCopied,
+    search,
+    setSearch
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainContext.Provider value={data}>
+        <Sidebar />
+        <Content />
+        {copied && <Copied color={copied} />}
+      </MainContext.Provider>
+    </>
   );
 }
 
---
+++ src/components/Brand.js
@@ -0,0 +1,48 @@
+import { getContrastYIQ } from "../helpers";
+import MainContext from "../MainContext";
+import { useContext } from "react";
+import ClipboardButton from "react-clipboard.js";
+
+function Brand({ brand }) {
+  const { selectedBrands, setSelectedBrands } = useContext(MainContext);
+  const { setCopied } = useContext(MainContext);
+
+  const toggleSelected = () => {
+    if (selectedBrands.includes(brand.slug)) {
+      setSelectedBrands(selectedBrands.filter((slug) => slug !== brand.slug));
+    } else {
+      setSelectedBrands([...selectedBrands, brand.slug]);
+    }
+  };
+
+  const setColor = (color) => {
+    setCopied(color);
+  };
+
+  return (
+    <div
+      className={`brand ${
+        selectedBrands.includes(brand.slug) ? "selected" : ""
+      }`}
+    >
+      <h5 onClick={toggleSelected}>{brand.title}</h5>
+      <div className="brand-colors">
+        {brand.colors.map((color) => (
+          <ClipboardButton
+            onSuccess={() => setColor(color)}
+            data-clipboard-text={color}
+            component="span"
+            style={{
+              "--bgColor": `#${color}`,
+              "--textColor": `${getContrastYIQ(color)}`,
+            }}
+          >
+            {color}
+          </ClipboardButton>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Brand;
